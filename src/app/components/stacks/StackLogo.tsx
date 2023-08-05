0ac5d866d8e164d80d4602977d3e3273bd12ce47
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-05 17:24:09 +0100

    refactor component stack Logo

--- src/app/components/stacks/StackLogo.tsx
+++ src/app/components/stacks/StackLogo.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image"
-import { ElementType } from "react"
+import ExpoLog from "../../../assets/logo-wordmark.svg"
 
 interface StackLogoProps{
-    image_link:string
+    image_link:string,
+    name:string
 }
 
-export function StackLogo({image_link}:StackLogoProps){
-    return <Image unoptimized  src={image_link} alt="LOGO_SCTACK" height={64} width={64}/>
+export function StackLogo({image_link, name}:StackLogoProps){
+    const url=name==="Expo"?ExpoLog:image_link
+    const bg=name==="Expo"||name==="Nextjs"?"bg-stone-700":"bg-stone-900"
+
+   
+
+    return(<div className={`flex items-center content-center justify-center w-[5rem] h-[5rem] rounded-lg ${bg}`}>
+            <Image  src={url} width={58} height={52} alt="logo_stack"/>          
+        </div>)
 }
