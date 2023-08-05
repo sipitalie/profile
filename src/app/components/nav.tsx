a076bb4d433dbbb657bf1c68b0c46ac47e189fd4
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-05 17:22:02 +0100

    add nav fixed and log

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,11 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import logoH from "../../../public/es.svg"
 
-export default function Nav({ children }:{children:React.ReactNode}) {
+export default function Nav({ children }:{children?:React.ReactNode}) {
     return (
-      <nav className="flex   text-sm font-medium">
-        <ul className="flex items-center space-x-6 ">
+      <nav className="flex fixed w-screen h-16 z-10 bg-stone-950 border-b border-stone-900   text-sm font-medium px-16 ">
+        <ul className="flex flex-row w-1/4 h-full">
+          <li className="flex flex-row items-center justify-center h-full" ><Link href={"/"}>
+            <div className="flex items-center content-center justify-center w-[3rem] h-[3rem] bg-stone-900 rounded-lg">
+            <Image className="w-full h-full" src={logoH} alt="estevao_sipitali"/>          
+          </div>
+          </Link></li>
+          <li className="flex flex-row items-center justify-center h-full" ><Link href={"/"}>
+          <div className="flex items-center justify-center h-full mx-2.5" >
+            <h1 className="text-white text-center text-[1.4rem] font-bold">Estevão Sipitali</h1>
+          </div>
+          </Link></li></ul>
+        <ul className="flex items-center  w-3/4">
           {children}
         </ul>
       </nav>
     )
-  }
+}
