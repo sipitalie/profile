503e4507550b747cdb808a780762868861fba171
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-05 17:24:30 +0100

    refactor component stack Root

--- src/app/components/stacks/StackRoot.tsx
+++ src/app/components/stacks/StackRoot.tsx
@@ -4,7 +4,9 @@ interface StackRootProps{
     children:ReactNode
 }
 export function StackRoot ({ children }:StackRootProps ){
-    return <li className="flex flex-row justify-center">
+    return <li className="flex w-[14rem] h-[6rem]   justify-center">
+        <div className="flex w-full h-full items-center mx-2.5 p-2 rounded-xl  justify-between border-2 border-stone-900">
         {children}
+        </div>
     </li>
 }
