68455bd3e3a5c5b7fe22af55b54d0584d2708921
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-05 17:22:40 +0100

    refactor component navItem

--- src/app/components/navItem.tsx
+++ src/app/components/navItem.tsx
@@ -1,17 +1,17 @@
 type NavItemProps={
-    href:string
-     isActive:boolean,
-    children:React.ReactNode
+  href:string
+   isActive:boolean,
+  children:React.ReactNode
 }
 export default function NavItem({ href, isActive, children }:NavItemProps) {
-    return (
-      <li>
-        <a
-          href={href}
-          className={`block px-3 py-2 border-solid border-2 rounded-xl border-red-600  rounded-md ${isActive ? 'bg-red-00 text-white' : 'bg-slate-50'}`}
-        >
-          {children}
-        </a>
-      </li>
-    )
-  }
+  return (
+    <li>
+      <a
+        href={href}
+        className={`${isActive ? 'bg-red-00 text-white mx-2.5' : 'bg-slate-50 mx-2.5'}`}
+      >
+        {children}
+      </a>
+    </li>
+  )
+}
