d0702e22ab4216fa06d860b9adbd15b547dd231d
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-05 17:25:19 +0100

    refactor layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import NavItem from './components/navItem'
+import Nav from './components/nav'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,7 +16,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black`}>{children}</body>
+      <body className={`${inter.className} bg-zinc-950`}>
+      
+      <main className="flex flex-col  bg-zinc-950 h-screen w-full">
+       <Nav>
+        <NavItem href={'/posts'} isActive><span className={`text-white text-lg font-bold`}>Postagens</span></NavItem>
+        <NavItem href={'/projects'} isActive><span className={`text-white text-lg font-bold`}>Projectos</span></NavItem>
+       </Nav>
+        {children}
+      </main>
+      </body>
     </html>
   )
 }
+
