fc0861e97701c580029fbbfe24710de1ddce7e53
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-28 01:37:19 +0100

    use img html tag

--- src/app/components/ImageProfile/index.tsx
+++ src/app/components/ImageProfile/index.tsx
@@ -4,7 +4,7 @@ export function ImageProfile(){
     return (
         <div className="flex w-full h-52 items-center justify-center">
             <div className="flex w-52 h-52 items-center justify-items-center bg-stone-950 rounded-[50%] border border-red-700">
-                <Image className="flex w-full h-full rounded-[50%]" src={sipitaliLogo} alt="sipitaliLogo"/>
+                <img className="flex w-full h-full rounded-[50%]" src={sipitaliLogo} alt="sipitaliLogo"/>
             </div>
         </div>
     )
