763f1f585aa9ca56acec7e1d8c9c359d123463bd
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-28 01:43:17 +0100

    move files in public folder

--- src/app/components/ImageProfile/index.tsx
+++ src/app/components/ImageProfile/index.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image"
-import sipitaliLogo from "../../../assets/sipitali.JPG"
+import sipitaliLogo from "../../../../public/sipitali.JPG"
 export function ImageProfile(){
     return (
         <div className="flex w-full h-52 items-center justify-center">
             <div className="flex w-52 h-52 items-center justify-items-center bg-stone-950 rounded-[50%] border border-red-700">
-                <img className="flex w-full h-full rounded-[50%]" src={sipitaliLogo} alt="sipitaliLogo"/>
+                <Image className="flex w-full h-full rounded-[50%]" src={sipitaliLogo} alt="sipitaliLogo"/>
             </div>
         </div>
     )
