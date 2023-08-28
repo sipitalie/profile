bbaf62a85db029b61dc116e8a2e2a2276f7fff01
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-28 01:51:07 +0100

    use png image in page links

--- src/app/components/ImageProfile/index.tsx
+++ src/app/components/ImageProfile/index.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image"
-import sipitaliLogo from "../../../../public/sipitali.JPG"
+import sipitaliLogo from "../../../../public/Logo.png"
 export function ImageProfile(){
     return (
         <div className="flex w-full h-52 items-center justify-center">
-            <div className="flex w-52 h-52 items-center justify-items-center bg-stone-950 rounded-[50%] border border-red-700">
-                <Image className="flex w-full h-full rounded-[50%]" src={sipitaliLogo} alt="sipitaliLogo"/>
+            <div className="flex w-52 h-52 items-center bg-white justify-center bg-stone-950 rounded-[50%] border border-red-700">
+                <Image className="flex w-36 h-36 rounded-lg" src={sipitaliLogo} alt="sipitaliLogo"/>
             </div>
         </div>
     )
