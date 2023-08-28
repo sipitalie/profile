62bfab460fa5cd27e0185fbf7e16e2e23549ac87
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-08-28 01:23:08 +0100

    create component Icon Link

---
+++ src/app/components/Icon/index.tsx
@@ -0,0 +1,17 @@
+import {FaLinkedin,FaUserCircle} from "../react-icons/fa"
+import {AiFillInstagram,AiFillYoutube}from "../react-icons/ai"
+export type IconName="LinkedIn"|"Portfolio"|"Instagram"|"Youtube"
+export function Icon({name,color}:{name:IconName, color:string}){
+    switch (name) {
+        case "LinkedIn":
+            return <FaLinkedin size={32} color={color} />
+        case "Instagram":
+            return <AiFillInstagram size={32} color={color}/>
+        case "Portfolio":
+            return <FaUserCircle size={32} color={color}/>
+        case "Youtube":
+            return < AiFillYoutube size={32} color={color}/>
+        default:
+            return null;
+    }
+}
